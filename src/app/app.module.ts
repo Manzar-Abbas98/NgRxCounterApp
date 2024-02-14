672b43f20b9ede9035779684f063c97eb50f8dc5
Manzar Abbas <100190110+Manzar-Abbas98@users.noreply.github.com>
2024-02-13 22:59:43 -0800

    configure NgRx Devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterbuttonComponent } from './component/counterbutton/counterbutton.component';
@@ -9,6 +8,7 @@ import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './shared/store/counter.reducer';
 import { CustomcounterComponent } from './component/customcounter/customcounter.component';
 import { FormsModule } from '@angular/forms';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -23,6 +23,7 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
 
     StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
   providers: [],
   bootstrap: [AppComponent],
